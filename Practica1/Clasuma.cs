4872fdfaa0e7d8277409d3495ffc7a57fe613d4b
Luis Carlos Castro <cd210902@gmail.com>
2022-08-31 08:29:40 -0600

    Agregar archivos de proyecto.

---
+++ Practica1/Clasuma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    internal class Clasuma
+    {
+        //campos propiedades, como es
+        private int n1;
+        private int n2;
+
+        //constructor
+        public Clasuma(int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+
+        // métodos, acción, lo que hace
+        public int sumar()
+        {
+            return (n1 + n2);
+        }
+    }
+}
